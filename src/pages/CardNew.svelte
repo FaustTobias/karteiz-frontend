8110760d557ede82a88d49518e1d618383604096
FaustTobias <fausttobias@users.noreply.github.com>
2021-05-31 10:03:48 +0200

    Add form to category new

--- src/pages/CardNew.svelte
+++ src/pages/CardNew.svelte
@@ -28,31 +28,33 @@
 </script>
 
 <Main>
-    <div class="text-2xl text-center mt-12 mb-4 px-4">Add card</div>
-    <div class="text-lg text-center mb-12 px-4">Create a new card!</div>
-    <div class="flex flex-col gap-4">
-        <div class="mx-4 flex flex-row gap-4 items-center">
-            <div class="w-20">Question</div>
-            <div class="relative flex-1 h-10">
-                <input
-                    type="text"
-                    class="absolute top-0 left-0 w-full h-full border-2 border-gray-300 focus:border-yellow-400 px-2 focus:outline-none"
-                    bind:value={question}
-                />
+    <form class="flex-1 overflow-auto flex flex-col" disabled={$isWorking} on:submit|preventDefault={create}>
+        <div class="text-2xl text-center mt-12 mb-4 px-4">Add card</div>
+        <div class="text-lg text-center mb-12 px-4">Create a new card!</div>
+        <div class="flex flex-col gap-4">
+            <div class="mx-4 flex flex-row gap-4 items-center">
+                <div class="w-20">Question</div>
+                <div class="relative flex-1 h-10">
+                    <input
+                        type="text"
+                        class="absolute top-0 left-0 w-full h-full border-2 border-gray-300 focus:border-yellow-400 px-2 focus:outline-none"
+                        bind:value={question}
+                    />
+                </div>
+            </div>
+            <div class="mx-4 flex flex-row gap-4 items-center">
+                <div class="w-20">Answer</div>
+                <div class="relative flex-1 h-10">
+                    <input
+                        type="text"
+                        class="absolute top-0 left-0 w-full h-full border-2 border-gray-300 focus:border-yellow-400 px-2 focus:outline-none"
+                        bind:value={answer}
+                    />
+                </div>
             </div>
         </div>
-        <div class="mx-4 flex flex-row gap-4 items-center">
-            <div class="w-20">Answer</div>
-            <div class="relative flex-1 h-10">
-                <input
-                    type="text"
-                    class="absolute top-0 left-0 w-full h-full border-2 border-gray-300 focus:border-yellow-400 px-2 focus:outline-none"
-                    bind:value={answer}
-                />
-            </div>
+        <div class="flex flex-row justify-center mt-8">
+            <Button text="Create card" disabled={$isWorking} />
         </div>
-    </div>
-    <div class="flex flex-row justify-center mt-8">
-        <Button text="Create card" on:click={create} disabled={$isWorking} />
-    </div>
+    </form>
 </Main>
